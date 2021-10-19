b921cb5492cded2a183a7eb3ccb25367379116cb
Rafał Małeska <r.maleska@gmail.com>
2021-10-19 11:34:06 +0200

    add: new kind version

--- install.sh
+++ install.sh
@@ -176,7 +176,7 @@ function helm {
 }
 # Kind
 function kind-install {
-  curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+  curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
   sudo chmod +x ./kind
   sudo mv ./kind /usr/bin/kind
   kind create cluster --name kind
